3d39f031f09f99d07d468e5bf274b46b562ad8fd
Nathan Withers <nathanwithz@gmail.com>
2018-05-15 09:50:48 -0400

    added IANA codes and finished up main interface

--- src/main/java/tech/thewithz/oxforddict/OxfordDictionary.java
+++ src/main/java/tech/thewithz/oxforddict/OxfordDictionary.java
@@ -5,20 +5,61 @@ import exceptions.*;
 public interface OxfordDictionary {
 
     public enum Language {
-        EN, ES, MS, SW, TN, NSO, LV, ID, UR, ZU, RO, HI; 
-    };
+        EN("en", "English"), 
+        ES("es", "Spanish"), 
+        MS("ms", "Malay"), 
+        SW("sw", "Swahili"), 
+        TN("tn", "Setswana"), 
+        NSO("nso", "Northern Sotho"), 
+        LV("lv", "Latvian"), 
+        ID("id", "Indonesian"), 
+        UR("ur", "Urdu"), 
+        ZU("zu", "isiZulu"), 
+        RO("ro", "Romanian"), 
+        HI("hi", "Hindu");
 
-    public enum Region {
+        private String name;
+        private String IANAcode;
 
-    };
+        Language(String IANAcode, String name) {
+            this.name = name;
+            this.IANAcode = IANAcode;
+        }
 
-    public Language defaultLanguage = Language.EN;
+        public String getName() {
+            return name;
+        }
 
-    public JsonObject setLanguage(String lang) throws UnsupportedLanguageException;
+        public String getIANAcode() {
+            return IANAcode;
+        }
+    }
 
-    public JsonObject lemmatron(String word, String... filters);
+    private final String BASE_URL = "https://od-api-demo.oxforddictionaries.com/api/v1";
 
-    // TODO dynamic enum for regions to use so arraylist is an option for parameter
-    public JsonObject entries();
+    private Language lang = Language.EN;
 
+    private String app_key = "";
+
+    private String app_id = "";
+
+    public void setLanguage(Language lang);
+
+    public void setCredentials(String app_key, String app_id);
+
+    public LemmatronRequestBuilder lemmatron(String word);
+
+    public EntriesRequestBuilder entries(String word);
+
+    // throws the exception if both antonyms and synonyms are false
+    public JsonObject thesaurus(String word, boolean antonyms, boolean synonyms) throws BadRequestException;
+
+    public SearchRequestBuilder search(String word);
+
+    public TranslationRequestBuilder translate(String word);
+
+    public WordlistRequestBuilder wordlist(String word);
+
+    // throws the exception if lang is anything but english
+    public JsonObject sentences(String word) throws UnsupportedLanguageException;
 }
